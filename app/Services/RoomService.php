a136e7ee1879cb8591b70bbe20f5ffff551c8bfa
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-09-07 06:49:23 +0700

    Add or make feature search private chat

--- app/Services/RoomService.php
+++ app/Services/RoomService.php
@@ -8,14 +8,24 @@ use App\Models\Room;
 
 class RoomService
 {
-    public function getRooms()
+    public function getRooms(string $search = null)
     {
         $user = Auth::user();
 
-        return $user->rooms()
+        $query = $user->rooms()
             ->with(['members' => function ($query) use ($user) {
                 $query->where('user_id', '!=', $user->id);
-            }])
-            ->get();
+            }]);
+
+        if ($search) {
+            $query->where(function ($q) use ($user, $search) {
+                $q->where('type', 'private')
+                    ->whereHas('members', function ($memberQuery) use ($user, $search) {
+                        $memberQuery->where('user_id', '!=', $user->id)
+                            ->where('name', 'like', '%' . $search . '%');
+                    });
+            });
+        }
+        return $query->get();
     }
 }
